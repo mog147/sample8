382f8f9e2f27c2e87594dd693c020bd38cc649ff
mog147 <396mog@gmail.com>
2025-01-16 22:18:18 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,11 +35,24 @@ $('.slide__full').slick('slickPlay');
 });
 
 // hamburger
-$(function(){
-    $(".btn-gNav").on("click",function(){
-       $(".gnav").toggleClass("open");
-    });
-  });
+// hamburger
+if(window.matchMedia("(max-width: 768px)").matches){
+    // ウィンドウサイズが 768px以下の場合のコードをここに
+
+      $(".btn-gNav").click(function(){
+        if($(".gnav").hasClass("open")){ // クリックされた要素がopenクラスだったら
+          $(".gnav").removeClass("open");
+        }else{
+          $(".gnav").addClass("open");
+        }
+      });
+
+      $(".gnav").click(function(){
+        $(this).removeClass("open");
+      });
+
+  }
+
 
 
 // 
